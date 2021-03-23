63550cbf5946375633b6434e4527ac80bc7c4d27
Pavel <impetus@yandex.ru>
2021-03-23 16:38:33 +0300

    vars

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyThread.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyThread.java
@@ -1,8 +1,33 @@
 package ru.samsung.itschool.mdev.myapplication;
 
+import android.animation.ArgbEvaluator;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+
 public class MyThread extends Thread {
+
+    // константа для интерполяции
+    public static final int FRACTION_TIME = 1000;
+
+    // переменная для интерполяции
+    private ArgbEvaluator argbEvaluator;
+
+    private Paint paint;
+
+    // указатель для SurfaceView
+    private SurfaceHolder holder;
+
+    private boolean flag;
+
+    private long startTime;
+    private long buffRedrawTime;
+
+    
+
+
+
     @Override
     public void run() {
-        super.run();
+
     }
 }
